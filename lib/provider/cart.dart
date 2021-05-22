4e97999ce5c9cc98dba33795afc6a9d78256ee80
MorDvash <mordvash1@gmail.com>
2021-05-22 17:10:45 +0300

    card badge

--- lib/provider/cart.dart
+++ lib/provider/cart.dart
@@ -15,10 +15,14 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  late Map<String, CartItem> _items;
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
   }
 
   void addItem(String productId, String title, double price,) {
@@ -35,6 +39,7 @@ class Cart with ChangeNotifier {
               quantity: 1,
               price: price));
     }
+    notifyListeners();
   }
 
 }
--- lib/screens/productOverviewScreen.dart
+++ lib/screens/productOverviewScreen.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/provider/cart.dart';
+import 'package:shop_app/widget/badge.dart';
 
 import '../widget/productsGridView.dart';
 
-enum FilterOptions{
-  Favorites,
-  All
-}
+enum FilterOptions { Favorites, All }
 
 class ProductOverviewScreen extends StatefulWidget {
-
   @override
   _ProductOverviewScreenState createState() => _ProductOverviewScreenState();
 }
 
 class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
   var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,16 +22,16 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
           title: Text('My-Shop'),
           actions: [
             PopupMenuButton(
-              onSelected: (FilterOptions selectedValue){
-                setState(() {
-                  if(selectedValue == FilterOptions.Favorites){
-                    _showOnlyFavorites = true;
-                  }else {
-                    _showOnlyFavorites = false;
-                  }
-                });
-              },
-              icon: Icon(Icons.more_vert),
+                onSelected: (FilterOptions selectedValue) {
+                  setState(() {
+                    if (selectedValue == FilterOptions.Favorites) {
+                      _showOnlyFavorites = true;
+                    } else {
+                      _showOnlyFavorites = false;
+                    }
+                  });
+                },
+                icon: Icon(Icons.more_vert),
                 itemBuilder: (_) => [
                       PopupMenuItem(
                         child: Text('Only Favorites'),
@@ -41,7 +41,20 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
                         child: Text('All'),
                         value: FilterOptions.All,
                       )
-                    ])
+                    ]),
+            Consumer<Cart>(
+              builder: (_, cart, ch) => Badge(
+                color: Theme.of(context).accentColor,
+                child: ch as Widget,
+                value: cart.itemCount.toString(),
+              ),
+              child: IconButton(
+                icon: Icon(
+                  Icons.shopping_cart,
+                ),
+                onPressed: () {},
+              ),
+            )
           ],
         ),
         body: ProductsGridView(_showOnlyFavorites));
