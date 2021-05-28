5cf6f8801c5571dcff8910f7184b2b6d1c5d0879
MorDvash <mordvash1@gmail.com>
2021-05-28 15:38:41 +0300

    focusNode

--- lib/screens/editProductScreen.dart
+++ lib/screens/editProductScreen.dart
@@ -11,6 +11,7 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   @override
   Widget build(BuildContext context) {
+    final _priceFocusNode = FocusNode();
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit product'),
@@ -23,6 +24,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  Focus.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
               )
             ],
           ),
